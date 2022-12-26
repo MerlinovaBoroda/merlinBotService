6635e9511dfcefd919720db9cfa2a34e13d29120
Merlinovaboroda <merlinovaboroda17@gmail.com>
2022-12-26 20:51:51 +0200

    mb fix

--- MerlinBot_Service/Services/MerlinBot.MessageHandler.cs
+++ MerlinBot_Service/Services/MerlinBot.MessageHandler.cs
@@ -16,18 +16,16 @@ public partial class MerlinBotService
             return;
         }
 
-        var appUser = message.From; // Save current user;
-        var hasText = !string.IsNullOrEmpty(message.Text); // True if message has text;
-
 #if DEBUG
         _logger.LogInformation("New message from chat id: {ChatId}", message.Chat.Id);
-        _logger.LogInformation("Message: {MessageContent}", hasText ? message.Text : "No text");
+        _logger.LogInformation("Message: {MessageContent}",
+            !string.IsNullOrEmpty(message.Text) ? message.Text : "No text");
 #endif
 
         // Only private Chats
         if (message.Chat.Type == ChatType.Private)
         {
-            Helpers.SavePrivateChat(appUser);
+            Helpers.SavePrivateChat(message.From);
         }
         else // Only group chats
         {
@@ -50,7 +48,7 @@ public partial class MerlinBotService
 
             //Karma count
             //Check if message is reply 
-            if (message.ReplyToMessage != null)
+            if (message.ReplyToMessage != null && message.Text != null)
             {
                 //Check if message is sticker
                 /*if (message.Sticker != null)
@@ -80,20 +78,22 @@ public partial class MerlinBotService
                 }*/
 
                 //Check if message starts with + or -
-                if (message.Text!.StartsWith("+") || message.Text.StartsWith("-"))
+                if (message.Text.StartsWith("+") || message.Text.StartsWith("-"))
                 {
                     if (!Helpers.CheckKarmaMessage(Api, message)) return;
                     //Add karma
-                    if (message.Text!.StartsWith("+") || message.Text.StartsWith("👍"))
+                    if (message.Text.StartsWith("+") || message.Text.StartsWith("👍"))
                     {
                         Helpers.SendPlusKarma(Api, message);
                     }
+
                     //Subtract karma
                     if (message.Text.StartsWith("-") || message.Text.StartsWith("👎"))
                     {
                         Helpers.SendMinusKarma(Api, message);
                     }
                 }
+                /*
                 //Check if message starts with like or dislike emoji
                 else if (message.Text.StartsWith("👍") || message.Text.StartsWith("👎"))
                 {
@@ -103,12 +103,13 @@ public partial class MerlinBotService
                     {
                         Helpers.SendPlusKarma(Api, message);
                     }
+
                     //Subtract karma
                     if (message.Text.StartsWith("-") || message.Text.StartsWith("👎"))
                     {
                         Helpers.SendMinusKarma(Api, message);
                     }
-                }
+                }*/
             }
         }
 
